5974fd18899b043fea641fe0e1b9d8441a44a036
Eric Friday <ericfriday@gmail.com>
2017-09-05 15:11:28 -0500

    counter and deleter functions operating

--- code-challenge/script.js
+++ code-challenge/script.js
@@ -12,14 +12,32 @@ var onReady = function() {
 $(document).ready(onReady);
 
 function createDiv() {
-  $('#container').append('<div class="color-div" ><p class="counter">' + numberOfClicks + '</p>' + '<button class="swap">Swap</button>' + '<button class="delete">Delete</button></p></div>');
-
+  console.log('appending div with buttons');  
+  $('#container').append('<div class="color-div" ><p class="counter">' 
+  + numberOfClicks 
+  + '</p>' 
+  + '<button class="swap">Swap</button>' 
+  + '<button class="delete">Delete</button>' 
+  + '</p></div>');
+  numberOfClicks ++;
+  $('.counter').on('click', counter);
+  $('.delete').on('click', deleter);
 }
 
 function counter() {
+  console.log('Counting clicks');
   $(".counter").html(numberOfClicks);
 }
 
+function deleter() {
+  console.log('deleting div');  
+  $(this).closest('div').remove();
+}
+
+function swapper() {
+  $('.swap').on('click', )
+  $('.swap').on('click', swapper);
+}
 // create button
 // create div element when clicked
 // inside that, create a p element that shows total count of times you've clicked that
