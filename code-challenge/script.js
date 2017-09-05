4a937c1d86c7e9d3868d9357b1b5a136ab27f941
Eric Friday <ericfriday@gmail.com>
2017-09-05 14:55:45 -0500

    creating divs with proper buttons

--- code-challenge/script.js
+++ code-challenge/script.js
@@ -4,8 +4,24 @@ var numberOfClicks = 0;
 
 var onReady = function() {
   console.log('the doc is ready');
+  $('#create').on('click', createDiv);
 
 };
 
 // When the DOM is loaded, our code in the onReady function will run.
 $(document).ready(onReady);
+
+function createDiv() {
+  $('#container').append('<div class="color-div" ><p class="counter">' + numberOfClicks + '</p>' + '<button class="swap">Swap</button>' + '<button class="delete">Delete</button></p></div>');
+
+}
+
+function counter() {
+  $(".counter").html(numberOfClicks);
+}
+
+// create button
+// create div element when clicked
+// inside that, create a p element that shows total count of times you've clicked that
+// create two buttons inside div -- Swap & Delete
+// clicking delete should remove the parent div container
