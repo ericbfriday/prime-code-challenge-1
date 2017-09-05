b3f7cd2b4c4b4b3eb8e296a35c953ee749def4c0
Eric Friday <ericfriday@gmail.com>
2017-09-05 15:29:14 -0500

    able to turn red backgrounds yellow, but not yellow background red

--- code-challenge/script.js
+++ code-challenge/script.js
@@ -13,33 +13,34 @@ $(document).ready(onReady);
 
 function createDiv() {
   console.log('appending div with buttons');  
-  $('#container').append('<div class="color-div" ><p class="counter">' 
+  numberOfClicks ++;
+  $('#container').append('<div class="color-div" ><p>' 
   + numberOfClicks 
   + '</p>' 
   + '<button class="swap">Swap</button>' 
   + '<button class="delete">Delete</button>' 
   + '</p></div>');
-  numberOfClicks ++;
   $('.counter').on('click', counter);
   $('.delete').on('click', deleter);
+  $('.swap').on('click', swapper);
 }
 
 function counter() {
-  console.log('Counting clicks');
+//  console.log('Counting clicks');
   $(".counter").html(numberOfClicks);
-}
+} // ending counter function
 
 function deleter() {
-  console.log('deleting div');  
+//  console.log('deleting div');  
   $(this).closest('div').remove();
-}
+} // ending deleter function
 
 function swapper() {
-  $('.swap').on('click', )
-  $('.swap').on('click', swapper);
-}
-// create button
-// create div element when clicked
-// inside that, create a p element that shows total count of times you've clicked that
-// create two buttons inside div -- Swap & Delete
-// clicking delete should remove the parent div container
+  if ($(this).closest('div').css('background-color', 'red')) {
+    $('.swap').on('click', $(this).closest('div').css('background-color', 'yellow'));    
+  }
+  else {
+    $('.swap').on('click', $(this).closest('div').css('background-color', 'red'));    
+  }
+
+} // ending swapper function
